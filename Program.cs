5e6abc7c9fd501b6ec416022b67c7001775edcc4
erik-sts17 <61998324+erik-sts17@users.noreply.github.com>
2020-08-21 15:58:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Algoritmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valor inicial: ");
+            int inicial = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor final: ");
+            int final = int.Parse(Console.ReadLine());
+            if (inicial < final)
+            {
+                for (int i = 0; i < final; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Número incial maior que o final");
+            }
+
+
+        }
+    }
+}
